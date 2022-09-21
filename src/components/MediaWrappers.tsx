ae5ba9b2c6bf6315cc6473fcd7a8295a04799406
nickadamson <naburner@protonmail.com>
2022-09-21 16:39:32 -0700

    fix: misc bugs in wrapper

--- src/components/MediaWrappers.tsx
+++ src/components/MediaWrappers.tsx
@@ -14,44 +14,44 @@ type WrapperProps = {
 
 export const ImageWrapper = React.forwardRef<HTMLImageElement, WrapperProps>(
   ({ src, alt, handleCalculatedRatio }, ref) => {
-  const onImageLoad = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-    const width = e.currentTarget.naturalWidth;
-    const height = e.currentTarget.naturalHeight;
-    const ratio = calcAspectRatio({ width, height });
-    handleCalculatedRatio(ratio);
-  };
+    const onImageLoad = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+      const width = e.currentTarget.naturalWidth;
+      const height = e.currentTarget.naturalHeight;
+      const ratio = calcAspectRatio({ width, height });
+      handleCalculatedRatio(ratio);
+    };
 
     const hiddenFromScreenReaders = alt ? false : true;
 
-  return (
-    <>
-      <img
-        css={css({
-          display: "block",
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          maxWidth: "100%",
-          maxHeight: "100%",
-        })}
-        ref={ref}
-        src={src}
+    return (
+      <>
+        <img
+          css={css({
+            display: "block",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            maxWidth: "100%",
+            maxHeight: "100%",
+          })}
+          ref={ref}
+          src={src}
+          onLoad={(e) => onImageLoad(e)}
           alt={alt}
           aria-hidden={hiddenFromScreenReaders}
-        // TODO let users configure loady after specified index
-        // loading="lazy"
-        onLoad={(loadedMedia) => onImageLoad(loadedMedia)}
-      />
-    </>
-  );
+          // TODO let users configure loady after specified index
+          // loading="lazy"
+        />
+      </>
+    );
   }
 );
 
 export const VideoWrapper = React.forwardRef<HTMLVideoElement, WrapperProps>(({ src, handleCalculatedRatio }, ref) => {
   const onVideoLoad = (e: React.SyntheticEvent<HTMLVideoElement, Event>) => {
     const width = e.currentTarget.videoWidth;
-    const height = e.currentTarget.videoWidth;
+    const height = e.currentTarget.videoHeight;
     const ratio = calcAspectRatio({ width, height });
     handleCalculatedRatio(ratio);
   };
@@ -69,10 +69,7 @@ export const VideoWrapper = React.forwardRef<HTMLVideoElement, WrapperProps>(({ 
       })}
       ref={ref}
       muted
-      onLoad={(e) => onVideoLoad(e)}
-      // TODO test these
-      // onLoadedData={(e) => onVideoLoad(e)}
-      // onLoadedMetadata={(e) => onVideoLoad(e)}
+      onLoadedMetadata={(e) => onVideoLoad(e)}
       autoPlay
       controls={false}
       playsInline
