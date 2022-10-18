23b0ffd94160dce7d2084b004856685e45baefaa
nickadamson <naburner@protonmail.com>
2022-10-18 16:58:27 -0700

    refactor: create constants.ts for jest tests

--- tests/components/MessonryGrid.test.tsx
+++ tests/components/MessonryGrid.test.tsx
@@ -1,32 +1,21 @@
 import { render, screen } from "@testing-library/react";
 
-import { Item, SupportedMimeTypes } from "../../src/components/GridItem";
 import { MessonryGrid } from "../../src/components/MessonryGrid";
-import images from "../../src/stories/images.json";
+import { testImages, testVideos, defaultOptions } from "../constants";
 
-const tenImages = images.slice(0, 10) as Item[];
-
-const videoLink = "https://infura-ipfs.io/ipfs/bafybeibjz7p2sbp2s7pyt6gu5ngf3dlwx3kc4d2wgpozq34oqpmckhioqq";
-const videos: Item[] = [
-  {
-    src: videoLink,
-    mimeType: SupportedMimeTypes.Video,
-  },
-];
-
-describe("MessonryGrid", () => {
+describe("MessonryGrid Component Test", () => {
   test("Should render the correct # of items", () => {
-    render(<MessonryGrid items={tenImages} options={{ placeholder: true }} />);
+    render(<MessonryGrid items={testImages} options={defaultOptions} />);
     expect(screen.queryAllByRole("img", { hidden: true }).length).toEqual(10);
   });
 
   test("Should render images correctly", () => {
-    render(<MessonryGrid items={images.slice(0, 1) as Item[]} options={{ placeholder: true }} />);
+    render(<MessonryGrid items={testImages.slice(0, 1)} options={defaultOptions} />);
     expect(screen.getByTestId("img-0")).toHaveClass("css-3ty7jz");
   });
 
   test("Should render videos correctly", () => {
-    render(<MessonryGrid items={videos} options={{ placeholder: true }} />);
+    render(<MessonryGrid items={testVideos} options={defaultOptions} />);
     expect(screen.getByTestId("video-0")).toHaveClass("css-3ty7jz");
   });
 });
---
+++ tests/constants.ts
@@ -0,0 +1,54 @@
+import { Item } from "src/components/GridItem";
+import { MessonryOptions } from "src/components/MessonryGrid";
+
+export const defaultOptions: MessonryOptions = { useNextImage: false };
+
+export const testImages = [
+  {
+    src: "https://images.unsplash.com/photo-1656025896950-8b6554141cce?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1656071601013-8abb438501fb?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1656370747240-97c9f260851d?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1656376406253-3794cdc050b1?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+  {
+    src: "https://imagas const;es.unsplash.com/photo-1656562105231-7d34cce4f123?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1656758713323-77e970801253?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1656872626959-d40665207e18?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1656919051982-a5718566de6f?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1658210613653-d813df02fba1?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1658299274928-b81f6c8a6b08?ixid=MnwzNDg4NTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTg1MjU4MzA&ixlib=rb-1.2.1",
+    mimeType: "image",
+  },
+] as Item[];
+
+export const testVideos = [
+  {
+    src: "https://infura-ipfs.io/ipfs/bafybeibjz7p2sbp2s7pyt6gu5ngf3dlwx3kc4d2wgpozq34oqpmckhioqq",
+    mimeType: "video",
+  },
+] as Item[];
