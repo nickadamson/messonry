1604311247f077718650435b4ca38ae87b41e55d
nickadamson <naburner@protonmail.com>
2022-11-04 10:11:14 -0700

    feat: fetchImageSize - get dimensions on server-side!!!
    Co-authored-by:
    Federico Brigante <me@fregante.com>
    https://github.com/image-size/image-size/issues/258#issuecomment-792493746

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const webpack = require("webpack");
 module.exports = {
   reactStrictMode: true,
   images: {
@@ -9,5 +10,26 @@ module.exports = {
         pathname: "/**",
       },
     ],
+  },
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      buffer: require.resolve("buffer/"),
+      fs: false,
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      stream: require.resolve("stream-browserify"),
+    };
+    config.plugins = [
+      ...config.plugins,
+      // Work around for Buffer is undefined:
+      // https://github.com/webpack/changelog-v5/issues/10
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+      }),
+    ];
+
+    return config;
   },
 };
